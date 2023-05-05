341fff0bb85fff8e3f791d0539d44efdbbb4f277
juliameireles <132387639+juliameireles@users.noreply.github.com>
2023-05-05 14:22:47 -0300

    Update lista1questao1.c

--- lista1questao1.c
+++ lista1questao1.c
@@ -1,4 +1,4 @@
-/*1.Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do
+/*QUESTÃO = 1.Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do
 produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
 Apresente o resultado da seguinte forma:
 DIFERENCA = (“valor de A” * “valor de B” - ““valor de C” * ““valor de D”)
