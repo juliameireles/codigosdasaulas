2e16019c83e51d399e116da291b6ceb169c8db2e
juliameireles <132387639+juliameireles@users.noreply.github.com>
2023-05-05 14:25:25 -0300

    Update lista1questao4.c

--- lista1questao4.c
+++ lista1questao4.c
@@ -1,7 +1,7 @@
 /*Professor, li que não para usar vetores assim que fui colocar os códigos no github,eu os usei
 apenas para deixar o código mais eficiente e enxuto. Está compilando perfeitamente. Mil perdões. 
 Prestarei mais atenção da próxima vez.
-QUESTÃO = Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas
+QUESTÃO = 4.Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas
 de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o ponto, ou
 se está sobre um dos eixos cartesianos ou na origem (x = y = 0). Se o ponto estiver na origem,
 escreva a mensagem “Origem”. Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou
