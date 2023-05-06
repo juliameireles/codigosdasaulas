ff0fd4872cb55331a2aa9ee5df8a8d7490cca9a1
juliameireles <132387639+juliameireles@users.noreply.github.com>
2023-05-05 22:36:34 -0300

    Update lista1questao2.c

--- lista1questao2.c
+++ lista1questao2.c
@@ -3,8 +3,7 @@ apenas para deixar o código mais eficiente e enxuto. Está compilando perfeitam
 Prestarei mais atenção da próxima vez.
 QUESTÃO = 2.Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no
 plano, p1(x1, y1) e p2(x2, y2) e calcule a distância entre eles, mostrando 4 casas decimais
-após a vírgula, segundo a fórmula:
-*/
+após a vírgula, segundo a fórmula:*/
 #include <stdio.h>
 #include <math.h>
     int main(){
